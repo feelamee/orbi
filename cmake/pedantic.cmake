05d9c4512b32980eadb197581aab219d66354922
feelamee <fosterlounch@gmail.com>
2024-10-25 17:46:09 +0300

    [build] add `/permissive-` option for MSVC

--- cmake/pedantic.cmake
+++ cmake/pedantic.cmake
@@ -5,7 +5,7 @@ if(NOT MSVC)
   target_compile_options(pedantic INTERFACE "-Wall" "-Werror" "-pedantic"
                                             "-Wshadow")
 else()
-  target_compile_options(pedantic INTERFACE "/W4" "/WX")
+  target_compile_options(pedantic INTERFACE "/W4" "/WX" "/permissive-")
 endif()
 
 add_library(pedantic::pedantic ALIAS pedantic)
