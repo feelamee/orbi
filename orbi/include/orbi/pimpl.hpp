b7909a0f7ae86ef749627331098a38e0b44e92de
feelamee <fosterlounch@gmail.com>
2024-11-24 23:02:37 +0200

    [orbi] add `pimpl`

---
+++ orbi/include/orbi/pimpl.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <array>
+
+template <class T, std::size_t Size, std::size_t Alignment>
+struct pimpl
+{
+public:
+    template <class... Args>
+    pimpl(Args&&... args)
+    {
+        std::construct_at(data.data(), std::forward<Args>(args)...);
+    }
+
+    ~pimpl()
+    {
+        validate();
+
+        std::destroy_at(data.data());
+    }
+
+    pimpl(pimpl const& other)
+        : pimpl(*other.as_held())
+    {
+    }
+
+    pimpl(pimpl&& other)
+        : pimpl(*other.as_held())
+    {
+    }
+
+    pimpl& operator=(pimpl&&) = delete;
+
+    pimpl&
+    operator=(pimpl other)
+    {
+        *as_held() = std::move(*other.as_held());
+    }
+
+    friend void
+    swap(pimpl& l, pimpl& r)
+    {
+        using std::swap;
+
+        swap(l.data, r.data);
+    }
+
+    consteval static void
+    validate()
+    {
+        static_assert(Size == sizeof(T));
+        static_assert(Alignment == alignof(T));
+    }
+
+    T*
+    operator->() noexcept
+    {
+        return as_held();
+    }
+
+    T const*
+    operator->() const noexcept
+    {
+        return as_held();
+    }
+
+    T&
+    operator*() noexcept
+    {
+        return *as_held();
+    }
+
+    T const&
+    operator*() const noexcept
+    {
+        return *as_held();
+    }
+
+private:
+    T const*
+    as_held() const noexcept
+    {
+        return reinterpret_cast<T const*>(data.data());
+    }
+
+    alignas(Alignment) std::array<std::byte, Size> data;
+};
