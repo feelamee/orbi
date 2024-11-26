95c620d1e2b480aec9beca9cf2c1136072b7763b
feelamee <fosterlounch@gmail.com>
2024-11-27 01:04:27 +0200

    [build] use libc++ in llvm toolchain

--- cmake/x86_64-linux-llvm.toolchain.cmake
+++ cmake/x86_64-linux-llvm.toolchain.cmake
@@ -6,6 +6,8 @@ set(CMAKE_LINKER_TYPE LLD)
 find_program(CMAKE_C_COMPILER NAMES clang)
 find_program(CMAKE_CXX_COMPILER NAMES clang++)
 
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+
 if(NOT CMAKE_C_COMPILER)
   message(FATAL_ERROR "Failed to find CMAKE_C_COMPILER.")
 endif()
